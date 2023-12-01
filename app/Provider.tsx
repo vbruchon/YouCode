aa0c1001b23e846b35b2c478f9305de746bc1772
vbruhon <vivian.bruchon@le-campus-numerique.fr>
2023-12-01 11:46:39 +0100

    T1 - Setup Project
    La stack :
    - Database: https://www.prisma.io/
    - Style: https://tailwindcss.com/ + https://ui.shadcn.com/
    - Auth: https://next-auth.js.org/
    - Query: https://tanstack.com/query/latest/docs/react/overview
    - Data: https://zod.dev/ + https://www.typescriptlang.org/

---
+++ app/Provider.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { Toaster, toast } from "sonner";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "next-themes";
+import { PropsWithChildren } from "react";
+
+const queryClient = new QueryClient();
+
+export const Providers = ({ children }: PropsWithChildren) => {
+  return (
+    <>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <Toaster />
+        <SessionProvider>{children}</SessionProvider>
+      </ThemeProvider>
+    </>
+  );
+};
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,31 @@
+import { SiteConfig } from "@/lib/site-config";
+import Link from "next/link";
+import { ThemeToggle } from "../theme/ThemeToggle";
+import { Typography } from "../ui/Typography";
+import Image from "next/image";
+
+export function Header() {
+  return (
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
+      <div className="container flex items-center h-16 space-x-4 sm:justify-between sm:space-x-0">
+        <div className="flex items-center gap-2 md:gap-10">
+          <Image
+            src={"/images/you-code.svg"}
+            width={50}
+            height={35}
+            alt="app logo"
+          />
+          <Typography variant="h3" as={Link} href="/">
+            {SiteConfig.title}
+          </Typography>
+        </div>
+
+        <div className="flex items-center justify-end flex-1 space-x-4">
+          <nav className="flex items-center space-x-1">
+            <ThemeToggle />
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
